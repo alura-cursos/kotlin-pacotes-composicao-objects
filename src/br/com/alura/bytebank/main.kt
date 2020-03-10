4dc20cc6b717b85bab8c1ccd322ef303de6c0672
Alex Felipe <alexfelipevieira@gmail.com>
2020-03-10 10:13:15 -0300

    3.2

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,14 +1,26 @@
 package br.com.alura.bytebank
 
-import br.com.alura.bytebank.modelo.Cliente
-import br.com.alura.bytebank.modelo.ContaCorrente
-import br.com.alura.bytebank.modelo.ContaPoupanca
-import br.com.alura.bytebank.modelo.totalContas
+import br.com.alura.bytebank.modelo.*
 import br.com.alura.bytebank.teste.testaContasDiferentes
 
 
 
 fun main() {
+
+    val fran = object : Autenticavel {
+        val nome: String = "Fran"
+        val cpf: String = "111.111.111-11"
+        val senha: Int = 1000
+
+        override fun autentica(senha: Int) = this.senha == senha
+    }
+
+    val sistemaInterno = SistemaInterno()
+    sistemaInterno.entra(fran, 1000)
+
+    println("nome do cliente ${fran.nome}")
+
+
     val alex = Cliente(nome = "Alex", cpf = "", senha = 1)
     val contaPoupanca = ContaPoupanca(titular = alex, numero = 1000)
     val contaCorrente = ContaCorrente(titular = alex, numero = 1001)
