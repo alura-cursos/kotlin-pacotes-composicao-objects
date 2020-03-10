0f2f9bb974b1733e26e5b99688a9b0e8154982fd
Alex Felipe <alexfelipevieira@gmail.com>
2020-03-10 09:38:07 -0300

    3.1

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,7 +1,19 @@
 package br.com.alura.bytebank
 
+import br.com.alura.bytebank.modelo.Cliente
+import br.com.alura.bytebank.modelo.ContaCorrente
+import br.com.alura.bytebank.modelo.ContaPoupanca
+import br.com.alura.bytebank.modelo.totalContas
 import br.com.alura.bytebank.teste.testaContasDiferentes
 
+
+
 fun main() {
+    val alex = Cliente(nome = "Alex", cpf = "", senha = 1)
+    val contaPoupanca = ContaPoupanca(titular = alex, numero = 1000)
+    val contaCorrente = ContaCorrente(titular = alex, numero = 1001)
+
     testaContasDiferentes()
+
+    println("Total de contas: $totalContas")
 }
--- src/br/com/alura/bytebank/modelo/Conta.kt
+++ src/br/com/alura/bytebank/modelo/Conta.kt
@@ -1,11 +1,19 @@
 package br.com.alura.bytebank.modelo
 
+var totalContas = 0
+    private set
+
 abstract class Conta(
     var titular: Cliente,
     val numero: Int
 ) {
     var saldo = 0.0
         protected set
+
+    init {
+        println("Criando conta")
+        totalContas++
+    }
 
     fun deposita(valor: Double) {
         if (valor > 0) {
@@ -22,5 +30,44 @@ abstract class Conta(
             return true
         }
         return false
+    }
+}
+
+class ContaCorrente(
+    titular: Cliente,
+    numero: Int
+) : Conta(
+    titular = titular,
+    numero = numero
+) {
+
+    init {
+        totalContas++
+    }
+
+    override fun saca(valor: Double) {
+        val valorComTaxa = valor + 0.1
+        if(this.saldo >= valorComTaxa){
+            this.saldo -= valorComTaxa
+        }
+    }
+}
+
+class ContaPoupanca(
+    titular: Cliente,
+    numero: Int
+) : Conta(
+    titular = titular,
+    numero = numero
+) {
+
+    init {
+        totalContas++
+    }
+
+    override fun saca(valor: Double) {
+        if(this.saldo >= valor){
+            this.saldo -= valor
+        }
     }
 }
