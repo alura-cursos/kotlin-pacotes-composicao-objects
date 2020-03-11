f49b9742355f7c7e6dc174bb86b72a100b71b612
Alex Felipe <alexfelipevieira@gmail.com>
2020-03-11 10:14:57 -0300

    4.3

--- src/br/com/alura/bytebank/modelo/CalculadoraBonificacao.kt
+++ src/br/com/alura/bytebank/modelo/CalculadoraBonificacao.kt
@@ -5,7 +5,7 @@ class CalculadoraBonificacao {
     var total: Double = 0.0
         private set
 
-    fun registra(funcionario: Funcionario){
+    fun registra(funcionario: Funcionario) {
         this.total += funcionario.bonificacao
     }
 
